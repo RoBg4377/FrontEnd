ee0b6081ea56e1a97fd0facb99b781d86ad218eb
RoBg4377 <rhobg4377@gmail.com>
2022-09-24 00:27:24 -0300

    Crea componentes: eduacación, experiencia,skills y proyectos.
    Crea barra de navegación colapsable, crea modulo de rutas y enruta las diferentes secciones.
    crea ruta por defecto

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,19 @@
-import { NgModule } from '@angular/core';
+import { Component, NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { ExperienciaComponent } from './componentes/experiencia/experiencia.component';
+import { EducacionComponent } from './componentes/educacion/educacion.component';
+import { AboutComponent } from './componentes/about/about.component';
+import { SkillsComponent } from './componentes/skills/skills.component';
+import { ProyectosComponent } from './componentes/proyectos/proyectos.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: '', redirectTo: 'inicio', pathMatch: 'full'},
+  {path: 'inicio', component: AboutComponent},
+  {path: 'experiencia', component: ExperienciaComponent},
+  {path: 'educacion', component: EducacionComponent},
+  {path: 'skills', component: SkillsComponent},
+  {path: 'proyectos', component: ProyectosComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,33 @@
 import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { HeaderComponent } from './componentes/header/header.component';
+import { FormContactoComponent } from './componentes/form-contacto/form-contacto.component';
+import { FormLoginComponent } from './componentes/form-login/form-login.component';
+import { AboutComponent } from './componentes/about/about.component';
+import { ExperienciaComponent } from './componentes/experiencia/experiencia.component';
+import { EducacionComponent } from './componentes/educacion/educacion.component';
+import { SkillsComponent } from './componentes/skills/skills.component';
+import { ProyectosComponent } from './componentes/proyectos/proyectos.component';
+
 
 @NgModule({
   declarations: [
     AppComponent,
-    HeaderComponent
+    FormContactoComponent,
+    FormLoginComponent,
+    AboutComponent,
+    ExperienciaComponent,
+    EducacionComponent,
+    SkillsComponent,
+    ProyectosComponent,
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
