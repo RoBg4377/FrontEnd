411ceb59420666df24b6d482227d12404e71b590
RoBg4377 <rhobg4377@gmail.com>
2023-01-08 13:12:53 -0300

    Crea dashboard y dinamizacion de las secciones. crea carpeta de logoSkils

--- src/app/componentes/educacion/educacion.component.ts
+++ src/app/componentes/educacion/educacion.component.ts
@@ -7,6 +7,16 @@ import { Component, OnInit } from '@angular/core';
 })
 export class EducacionComponent implements OnInit {
 
+  instit: string = 'FreeCodeCamp Academy';
+  nomCurso: string = 'Algortimos de JavaScript y Estructura de datos';
+  temas: String[] = [
+    'JavaScript básico',
+    'ES6',
+    'Expresiones regulares',
+    'Depuración',
+    'Estructuras de datos básicas',
+  ];
+
   constructor() { }
 
   ngOnInit(): void {
