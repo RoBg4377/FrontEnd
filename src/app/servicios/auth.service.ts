3a3dac6daa23ab126d43ee8eb62eec2a7c51dcbf
RoBg4377 <rhobg4377@gmail.com>
2023-03-16 00:24:56 -0300

    cambia la url del backend

--- src/app/servicios/auth.service.ts
+++ src/app/servicios/auth.service.ts
@@ -10,7 +10,7 @@ import { NuevoUsuario } from '../model/nuevo-usuario';
 })
 export class AuthService {
 
-  authURL = 'http://localhost:8080/auth/';
+  authURL = 'https://rbg-backend.onrender.com/auth/';
 
   constructor(private http: HttpClient) { }
 
