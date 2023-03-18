3d868bb761c19826709128a55c430a15b5e82357
RoBg4377 <rhobg4377@gmail.com>
2023-03-17 23:11:54 -0300

    crea spin de carga.

--- src/app/servicios/interceptor.service.ts
+++ src/app/servicios/interceptor.service.ts
@@ -1,6 +1,7 @@
 import { HttpEvent, HttpHandler, HttpInterceptor, HttpRequest, HTTP_INTERCEPTORS } from '@angular/common/http';
 import { Injectable } from '@angular/core';
-import { Observable } from 'rxjs';
+import { finalize, Observable } from 'rxjs';
+import { SpinerService } from './spiner.service';
 import { TokenService } from './token.service';
 
 @Injectable({
@@ -9,18 +10,20 @@ import { TokenService } from './token.service';
 export class InterceptorService implements HttpInterceptor{
   
   constructor(
-    private tokenService : TokenService
+    private tokenService : TokenService,
+    private spinerService : SpinerService
   ) { 
     this.verificar()
     
   }
   intercept(req: HttpRequest<any>, next: HttpHandler): Observable<HttpEvent<any>> {
+    this.spinerService.mostrar();
     let intReq = req;
     const token = this.tokenService.getToken();
     if(token != null){
       intReq = req.clone({headers: req.headers.set('Authotization', 'Bearer ' + token)})
     }
-    return (next.handle(intReq));
+    return (next.handle(intReq).pipe(finalize(()=> this.spinerService.ocultar())));
   }
 
   verificar( ){
